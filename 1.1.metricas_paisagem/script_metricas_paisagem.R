85209da4eb196cba3a0220e52e03c87fb1e5a7cd
Everton A. Maciel <50334341+evertonmaciel@users.noreply.github.com>
2025-10-18 09:57:14 -0300

    Update script_metricas_paisagem.R

--- 1.1.metricas_paisagem/script_metricas_paisagem.R
+++ 1.1.metricas_paisagem/script_metricas_paisagem.R
@@ -34,12 +34,12 @@ plot(data)
 check_landscape(data)
 
 # métricas no nível de manchas
-resultado_machas <-calculate_lsm(data, level = "patch")
+resultados_manchas <-calculate_lsm(data, level = "patch")
 write.csv(resultados_manchas,"class_result.csv")
 
 # métricas no nível de classe
-resultados_classe <-calculate_lsm(data, level = "class")
-write.csv(resultados_classe,"class_result.csv")
+resultados_classes <-calculate_lsm(data, level = "class")
+write.csv(resultados_classes,"class_result.csv")
 
 # métricas no nível de paisagem
 resultados_paisagem <-calculate_lsm(data, level = "landscape")
@@ -60,14 +60,14 @@ library(tidyr)
 library(ggplot2)
 
 # excluir as colunas que não fazem sentido
-resultados_classe$layer <- NULL
-resultados_classe$id <- NULL
+resultados_classes$layer <- NULL
+resultados_classes$id <- NULL
 
 #excluir os NAs da coluna value
-resultados_classe <- resultados_classe[!is.na(resultados_classe$value), ]
+resultados_classes <- resultados_classes[!is.na(resultados_classes$value), ]
 
 # remover os desvios padroes e os coeficietes de variacoes
-result <- resultados_classe%>% 
+result <- resultados_classes%>% 
   filter(!grepl("(_cv|_sd)", metric))
 
 ## TESTE DE CORRELACAO COM TODAS AS METRICAS
@@ -95,6 +95,7 @@ ggplot(cor_df, aes(x = Metric1, y = Metric2, fill = Correlation)) +
   theme(axis.text.x = element_text(angle = 45, vjust = 1, hjust = 1)) +
   labs(title = "Spearman Correlation between Metric Parameters", x = "", y = "")
 dev.off() 
+
 
 
 
